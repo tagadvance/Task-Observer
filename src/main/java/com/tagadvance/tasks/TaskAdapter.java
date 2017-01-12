a21d048a776e138c7198386acce95a7223d99a75
Tag <tagadvance@gmail.com>
2017-01-11 17:47:13 -0800

    TaskAdapter

---
+++ src/main/java/com/tagadvance/tasks/TaskAdapter.java
@@ -0,0 +1,24 @@
+package com.tagadvance.tasks;
+
+public class TaskAdapter implements TaskListener {
+
+	public TaskAdapter() {
+
+	}
+
+	@Override
+	public void taskInitialized(TaskEvent e) {
+
+	}
+
+	@Override
+	public void taskCreated(TaskEvent e) {
+
+	}
+
+	@Override
+	public void taskDestroyed(TaskEvent e) {
+
+	}
+
+}
